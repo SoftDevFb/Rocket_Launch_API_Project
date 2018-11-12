2d03d8768b28e9f597229c9fd952b75afddbde81
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-11-12 12:01:16 -0600

    Added Header Next Launch Info

--- app.js
+++ app.js
@@ -7,13 +7,14 @@
 var httpRequest = new XMLHttpRequest();
 var launchInfoRow = document.getElementById("launchRow");
 var counterRow = document.getElementById("timerRow");
+var bannerLaunch = document.getElementById("underTitle");
 var btn1 = document.getElementById("allBtn");
 var btn2 = document.getElementById("falconBtn");
 var btn3 = document.getElementById("arianeBtn");
 var btn4 = document.getElementById("launcherOneBtn");
 
 //Get next 5 launches function linking to correct button
-btn1.addEventListener("click", function() {
+btn1.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
     ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?");
     ourRequest.onload = function () {
@@ -26,7 +27,7 @@ btn1.addEventListener("click", function() {
 //Get next 5 falcon launches function linking to correct button
 btn2.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?name=falcon");
+    ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?&name=falcon");
     ourRequest.onload = function () {
         var falconData = JSON.parse(ourRequest.responseText);
         renderHTML(falconData);
@@ -37,7 +38,7 @@ btn2.addEventListener("click", function () {
 //Get next 5 ariane launches function linking to correct button
 btn3.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?name=ariane");
+    ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?&name=ariane");
     ourRequest.onload = function () {
         var arianeData = JSON.parse(ourRequest.responseText);
         renderHTML(arianeData);
@@ -48,7 +49,7 @@ btn3.addEventListener("click", function () {
 //Get next 5 launcherone launches function linking to correct button
 btn4.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?name=launcherone");
+    ourRequest.open("get", "https://launchlibrary.net/1.4/launch?next=5?&name=launcherone");
     ourRequest.onload = function () {
         var launcherOneData = JSON.parse(ourRequest.responseText);
         renderHTML(launcherOneData);
@@ -66,4 +67,8 @@ function renderHTML(data) {
     } //end of for
 
     launchInfoRow.insertAdjacentHTML('beforeend', htmlString);
+    
+    //Places "Next Launch:"" under Page Header
+    bannerLaunch.insertAdjacentHTML('beforeend', "<p>" + "Next Launch: " + 
+    data.launches[0].net + data.launches[0].name + "</p>");  
 }
